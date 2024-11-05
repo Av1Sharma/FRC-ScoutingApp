229feed2f66f6425723db3da975842ea73de408c
Avi Sharma <asharmava1@gmail.com>
2024-11-05 13:09:50 -0500

    init commit

---
+++ FRCScoutingApp/FRCScoutingApp/FRCScoutingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRCScoutingAppApp.swift
+//  FRCScoutingApp
+//
+//  Created by Avi Sharma on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FRCScoutingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FRCScoutingApp/FRCScoutingAppTests/FRCScoutingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FRCScoutingAppTests.swift
+//  FRCScoutingAppTests
+//
+//  Created by Avi Sharma on 11/5/24.
+//
+
+import Testing
+@testable import FRCScoutingApp
+
+struct FRCScoutingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
